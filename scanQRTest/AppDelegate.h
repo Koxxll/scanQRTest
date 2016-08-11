4f6fc8fb872635c51e1ad2ce3dffb8a9d2908f44
koxx <912662036@qq.com>
2016-08-11 14:34:48 +0800

    Initial Commit

---
+++ scanQRTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  scanQRTest
+//
+//  Created by Afei on 16/8/11.
+//  Copyright © 2016年 梁玉玺. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ scanQRTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  scanQRTest
+//
+//  Created by Afei on 16/8/11.
+//  Copyright © 2016年 梁玉玺. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
